331485bae329cf21aee5b0f2039d8a67e7383cb8
sevrinbe <sevrin.eldred@gmail.com>
2022-08-12 22:06:58 -0500

    more progress on app

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,63 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title of the application?",
+        name: "appTitle",
+    },
+    {
+        type: "input",
+        message:  "Please enter a description of the project.",
+        name: "projectDesc",
+    },
+    {
+        type: "input",
+        message: "What is needed for installation?",
+        name: "installInstruct",
+    },
+    {
+        type: "input",
+        message: "What is the use of this project?",
+        name:  "usageInput"
+    },
+    {
+        type: "input",
+        message: "How does someone execute/start the program?",
+        name: "howToStart",
+    },
+    {
+        type: "input",
+        message: "Who are the authors on this project?",
+        name: "typedAuthors"
+    },
+    {
+        type: "list",
+        message: "Which license do you want to use?",
+        choices: ["MIT", "Apache License 2.0", "GNU General Public License v3.0", "BSD 2-Clause 'Simplified' License" ],
+        name: "licenseType",
+    },
+];
+
+var answers = [];
+
+var metaQuestions = [];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((responses) => {
+        generateMarkdown(responses);
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,8 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  console.log("words")
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,7 +14,63 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.appTitle}
+
+  ## Description
+  
+  ${projectDesc}
+  
+  ## Table of Contents (Optional)
+  
+  If your README is long, add a table of contents to make it easy for users to find what they need.
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+  
+  ${installInstruct}
+
+  ## Usage
+  
+  ${usageInput}
+  
+  To add a screenshot, create an  folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+  ## Starting the Program
+
+  ${howToStart}
+
+  ## Authors
+  
+  ${typedAuthors}
+  
+  ## License
+  
+  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+  
+  ---
+  
+  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+  
+  ## Badges
+  
+  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+  
+  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  
+  ## Features
+  
+  If your project has a lot of features, list them here.
+  
+  ## How to Contribute
+  
+  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+  
+  ## Tests
+  
 
 `;
 }
