729a09d936c5ffb1b8c221ac99051e407fb7eff9
Sevrin B Eldred <105235294+sevrinbe@users.noreply.github.com>
2022-08-28 14:59:56 -0500

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -45,7 +45,6 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    console.log("dicks")
     fs.writeFile(fileName, data, (err) =>
     err ? console.log(err) : console.log("Success!"));
 }
